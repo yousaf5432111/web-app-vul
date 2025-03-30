d609a18045d36de9dbda2ee60a3af46876839136
yousaf <yousaf5432111@gmail.com>
2025-03-30 19:29:38 +0500

    Update LessonController.php

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Lesson;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LessonController extends Controller
 {
@@ -18,4 +19,45 @@ class LessonController extends Controller
         $lesson = Lesson::findOrFail($id);
         return response()->json($lesson);
     }
+    public function store(Request $request)
+    {
+    
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|string|max:255',
+        'content' => 'required|string',
+        'resources' => 'required|json',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
+    }
+
+    $lesson = Lesson::create($request->all());
+    return response()->json($lesson, 201);
+}
+
+public function update(Request $request, $id)
+{
+    $lesson = Lesson::findOrFail($id);
+
+    $validator = Validator::make($request->all(), [
+        'title' => 'sometimes|required|string|max:255',
+        'content' => 'sometimes|required|string',
+        'resources' => 'sometimes|required|json',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
+    }
+
+    $lesson->update($request->all());
+    return response()->json($lesson);
+}
+
+public function destroy($id)
+{
+    $lesson = Lesson::findOrFail($id);
+    $lesson->delete();
+    return response()->json(['message' => 'Lesson deleted successfully']);
+}
 }
